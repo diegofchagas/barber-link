ea0546880c797e429b3519a65e3851c551a62aef
Diego Chagas <diegofchagas@gmail.com>
2025-08-28 12:06:44 -0300

    fix: ajustando dados para receber imagens

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
