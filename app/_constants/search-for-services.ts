d1b0c2d3827a8d2fac9f3e1795d78cb04f84c60e
Diego Chagas <diegofchagas@gmail.com>
2025-08-28 12:09:03 -0300

    feat: add novos componentens do shadcn

---
+++ app/_constants/search-for-services.ts
@@ -0,0 +1,31 @@
+interface SearchForServicesProps {
+  imageUrl: string
+  title: string
+}
+
+export const searchForServices: SearchForServicesProps[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratação",
+  },
+  {
+    imageUrl: "/Massagem.svg",
+    title: "Massagem",
+  },
+  {
+    imageUrl: "/Sobrancelha.svg",
+    title: "Sobrancelha",
+  },
+]
